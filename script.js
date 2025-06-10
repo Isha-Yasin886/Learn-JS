a633961acc7560d1973a27487b4fe3be022bda67
iramyaseen <199124444+Isha-Yasin886@users.noreply.github.com>
2025-06-10 11:33:56 +0500

    update

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-// let modebtn = document.querySelector("#mode");
-// let body = document.querySelector("body");
-// let curmode = "light";
+let modebtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let curmode = "light";
 
-// modebtn.addEventListener("click", () => {
-//     if (curmode === "light") {
-//         curmode = "dark";
-//         body.classList.add("dark");
-//         body.classList.remove("light");
-//     } else {
-//         curmode = "light";
-//         body.classList.add("light");
-//         body.classList.remove("dark");
-//     }
-//     console.log(curmode);
-// });
-// console.log("hy");
-// setTimeout(() =>{
-    // console.log("Hello!");
-// } , 2000);
-// function hello(){
-//     console.log("Hello isha!");
-// }
-// setTimeout(hello , 2000);
-// console.log("Delay ");
+modebtn.addEventListener("click", () => {
+    if (curmode === "light") {
+        curmode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    } else {
+        curmode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(curmode);
+});
+console.log("hy");
+setTimeout(() =>{
+    console.log("Hello!");
+} , 2000);
+function hello(){
+    console.log("Hello isha!");
+}
+setTimeout(hello , 2000);
+console.log("Delay ");
 
-// function sum (a,b){
-// console.log(a+b);
-// }
-// function calcul(a,b,sumCallBack){
+function sum (a,b){
+console.log(a+b);
+}
+function calcul(a,b,sumCallBack){
 
-//     sumCallBack(a,b);
-// }
-// calcul(3,4,sum);
-// console.log("Hello");
-// const URL = "https://cat-fact.herokuapp.com/facts";
-// const getFacts = async () => {
-//     console.log("Getting data....");
-//     let response = await fetch(URL);
-//     console.log(response.status);
-// };
+    sumCallBack(a,b);
+}
+calcul(3,4,sum);
+console.log("Hello");
+const URL = "https://cat-fact.herokuapp.com/facts";
+const getFacts = async () => {
+    console.log("Getting data....");
+    let response = await fetch(URL);
+    console.log(response.status);
+};
 
 // odd even number
 // for(let i = 0; i<=10;i++){
@@ -49,8 +49,8 @@
 
 //    table
 
-let n = prompt("Enter number for table:");
-n = parseInt(n);
-for (let i = 0; i <= 10; i++) {
-  console.log(`${n} * ${i} = ${n * i}`);
-}
+// let n = prompt("Enter number for table:");
+// n = parseInt(n);
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`${n} * ${i} = ${n * i}`);
+// }
